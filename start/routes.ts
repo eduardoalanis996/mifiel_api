a220a6b78cf3890bf6df836d464601288e68b0c0
eduardoalanis996 <eduardoalanis996@gmail.com>
2023-11-04 19:53:51 -0600

    First commit

---
+++ start/routes.ts
@@ -0,0 +1,36 @@
+/*
+|--------------------------------------------------------------------------
+| Routes
+|--------------------------------------------------------------------------
+|
+| This file is dedicated for defining HTTP routes. A single file is enough
+| for majority of projects, however you can define routes in different
+| files and just make sure to import them inside this file. For example
+|
+| Define routes in following two files
+| ├── start/routes/cart.ts
+| ├── start/routes/customer.ts
+|
+| and then import them inside `start/routes.ts` as follows
+|
+| import './routes/cart'
+| import './routes/customer'
+|
+*/
+
+import Route from '@ioc:Adonis/Core/Route'
+
+Route.get('/', async () => {
+  return { hello: 'world' }
+})
+
+
+Route.post('/sign_callback_url', async ({request, response})=>{
+  console.log('sign_callback_url')
+  console.log( request.body())
+})
+
+Route.post('/callback_url', async ({request, response})=>{
+  console.log('callback_url')
+  console.log( request.body())
+})
